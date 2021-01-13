5e4e5d1f4a38981cc1a435ace4c2b3879c7096be
mmf2fmm <77151781+mmf2fmm@users.noreply.github.com>
2021-01-13 22:59:02 +0300

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+Name = input("Kindly Write your name ")
+print(" Welcome Mr "+  Name )
+number1 = float(input("enter a number "))
+number2 = float(input("enter a second number "))
+print(number1 + number2)
+
+
+# In[ ]:
+
+
+
+
